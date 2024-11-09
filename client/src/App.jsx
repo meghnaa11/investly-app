bc9790a0d7b5d4b03d577c407fc35cfafc11ef3c
Ishan Garg <ishan.garg10394@gmail.com>
2024-11-09 18:49:09 -0500

    Investly - Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Header from '../components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header></Header>
       <h1>Investly</h1>
     </>
   )
