c4ebd62c8b900c63ed32d2f4c4eed7bf8383a599
Meghna Agrawal <meghnaagrawal10@gmail.com>
2024-11-09 19:42:36 -0500

    added client routing

---
+++ client/src/Components/Profile.jsx
@@ -0,0 +1,10 @@
+// on the basis of the url it will render different components- add wallet, details, portfolio
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
